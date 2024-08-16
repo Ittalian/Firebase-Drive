cbf8adf30d72ce82959445e0e0de61ca1f70a6d2
Ittalian <bothmather@au.com>
2024-08-17 00:34:13 +0900

    微修正

--- lib/models/picture_category.dart
+++ lib/models/picture_category.dart
@@ -1,23 +1,20 @@
 class PictureCategory {
   final String categoryId;
-  final String name;
   final String value;
 
   PictureCategory({
     required this.categoryId,
-    required this.name,
     required this.value,
   });
 
   factory PictureCategory.fromMap(Map<String, dynamic> map, String categoryId) {
     return PictureCategory(
-        categoryId: categoryId, name: map['name'] ?? '', value: map['value'] ?? '');
+        categoryId: categoryId, value: map['value'] ?? '');
   }
 
   Map<String, dynamic> toMap() {
     return {
       'category_id': categoryId,
-      'name': name,
       'value': value,
     };
   }
